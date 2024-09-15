c7ad1a1761aac172771726f70be74b11e25e6cbe
pshiva911 <shivakrishna911@gmail.com>
2024-09-15 13:43:45 +0530

    baseURL correction

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const axiosDf = axios.create({
 	// baseURL: 'https://jira-clone.onrender.com/',
-	baseURL: "http://ec2-65-2-63-115.ap-south-1.compute.amazonaws.com/:8080/",
+	baseURL: "http://ec2-65-2-63-115.ap-south-1.compute.amazonaws.com:8080/",
 	// baseURL: "http://localhost:4000/",
 	withCredentials: true,
 })
