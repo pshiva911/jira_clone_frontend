bc2d6683e0f7c2093940cae9aef7a7bb611a8548
pshiva911 <shivakrishna911@gmail.com>
2024-09-30 00:59:27 +0530

    changed base url

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,8 +3,8 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const api = createApi({
 	reducerPath: 'jiraApiReducer',
 	baseQuery: fetchBaseQuery({
-		// baseUrl: "http://ec2-65-2-63-115.ap-south-1.compute.amazonaws.com:8080/api",
-		baseUrl: "http://localhost:4000/api",
+		baseUrl: "http://ec2-65-2-63-115.ap-south-1.compute.amazonaws.com:8080/api",
+		// baseUrl: "http://localhost:4000/api",
 		credentials: 'include',
 	}),
 	tagTypes: ['Lists', 'Issues', 'Project', 'Members', 'AuthUser', 'Comments'],
