82be4ad8caacad77aead42881c632d1ee965b496
pshiva911 <shivakrishna911@gmail.com>
2024-10-03 00:02:49 +1300

    namemodified2

--- src/components/project/Details.tsx
+++ src/components/project/Details.tsx
@@ -1,7 +1,7 @@
 const Details = () => {
   return (
     <div className="mx-10">
-      <h1 className='mb-4 mt-7 text-xl font-semibold text-light-c-3'>JiraMini Board</h1>
+      <h1 className='mb-4 mt-7 text-xl font-semibold text-light-c-3'>Kanban Board</h1>
     </div>
   );
 };
