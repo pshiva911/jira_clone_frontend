d33bf88303ef021dcae8fe9a516829138505bb10
pshiva911 <shivakrishna911@gmail.com>
2024-10-03 18:50:28 +1300

    namemodified4

--- src/components/project/Project.tsx
+++ src/components/project/Project.tsx
@@ -33,7 +33,7 @@ const Project = () => {
 
   return (
     <div className='mt-6 flex grow flex-col px-8 sm:px-10'>
-      <h1 className='mb-4 text-xl font-semibold text-c-text'> Jira Board</h1>
+      <h1 className='mb-4 text-xl font-semibold text-c-text'> Kanban Board</h1>
       <Filter isEmpty={lists?.length === 0} {...{ projectId, setIsDragDisabled }} issueFilter={issueFilter} setIssueFilter={setIssueFilter} />
 
       {lists ? (
